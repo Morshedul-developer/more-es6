f7692d0814b15885ab89e3d18e2e83b04535b6ec
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-27 11:06:48 +0600

    explored about primitive and non-primitive data types

---
+++ primitive_non-primitive.js
@@ -0,0 +1,10 @@
+// Primitive data types in JavaScript include:
+let stringExample = "Hello, World!";
+let numberExample = 42;
+let booleanExample = true;
+let nullExample = null;
+let undefinedExample;
+
+// Non-primitive data types in JavaScript include:
+let objectExample = { name: "Alice", age: 30 };
+let arrayExample = [1, 2, 3, 4, 5];
