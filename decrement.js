870d926b159b78ba612a66e84340bf316bf9555a
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-28 22:37:53 +0600

    explored increment, decrement

---
+++ decrement.js
@@ -0,0 +1,7 @@
+let a = 1;
+a--; // This will throw an error because 'a' is a constant and cannot be reassigned.
+
+// console.log(a--); // Output: 0
+// console.log(a); // Output: -1
+
+console.log(--a); // Output: -1
---
+++ increment.js
@@ -0,0 +1,7 @@
+let a = 1;
+a++; // This will throw an error because 'a' is a constant and cannot be reassigned.
+
+// console.log(a++); // Output: 2
+// console.log(a); // Output: 3
+
+console.log(++a); // Output: 3
